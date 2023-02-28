e2a5827abf4f7a5ecfb454c6bc32e52db28afdda
Sean Gravener <sean@gravener.net>
2023-02-28 17:32:33 -0500

    Add solution

---
+++ misc/findsum.js
@@ -0,0 +1,20 @@
+function findSum(arr, value) {
+  let result = false;
+
+  for (let [i, a] of arr.entries()) {
+    const prevA = a;
+
+    for (let [i, b] of arr.entries()) {
+      const prevB = b;
+
+      if (a + b === value) {
+        result = [a, b];
+        break;
+      }
+    }
+  }
+
+  return result;
+}
+
+console.log(findSum([1, 21, 3, 14, 5, 60, 7, 6], 81));
