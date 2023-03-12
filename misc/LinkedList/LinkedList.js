e4914f2c3445a7ac138858c80b9f3497a372bc83
Sean Gravener <sean@gravener.net>
2023-03-12 18:40:27 -0400

    Insert nodes into list at head position

--- misc/LinkedList/LinkedList.js
+++ misc/LinkedList/LinkedList.js
@@ -1,9 +1,33 @@
+import { Node } from "./Node.js";
+
 export class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
   isEmpty() {
     return this.head === null;
   }
 
-  constructor() {
-    this.head = null;
+  insertAtHead(data) {
+    const node = new Node(data);
+    node.nextElement = this.head;
+    this.head = node;
+
+    return this;
+  }
+
+  printList() {
+    let currentHead = this.head;
+    let messages = [];
+
+    if (this.isEmpty()) return;
+
+    while (currentHead !== null) {
+      messages.push(currentHead.data);
+      currentHead = currentHead.nextElement;
+    }
+
+    console.log(messages.join(" -> "));
   }
 }
--- misc/LinkedList/index.js
+++ misc/LinkedList/index.js
@@ -1,5 +1,15 @@
+// Demo of a singly linked-list
+
 import { Node } from "./Node.js";
 import { LinkedList } from "./LinkedList.js";
 
 let list = new LinkedList();
 console.log(list.isEmpty());
+
+
+for (var i = 0; i < 10; i++) {
+  list = list.insertAtHead(i);
+}
+
+console.log(list);
+list.printList()
